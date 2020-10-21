be5723756ae0a7965960bba5ec4253edcbe0f8c0
Gabriella Domingues Lopes <69319644+GabriellaDomingues@users.noreply.github.com>
2020-10-21 00:56:47 -0300

    Observação sobre jornal.sql !!!
    Observação sobre o arquivo jornal.sql.

---
+++ 4° BIMESTRE - ETEC/Banco de dados - JORNAL/README - Sobre jornal.sql
@@ -0,0 +1,5 @@
+O arquivo pode perder a formatação caso for aberto no Bloco de Notas!!
+Caso isso aconteça, abra o arquivo (jornal.sql) pelo WordPad ou pelo navegador.
+
+Se seu objetivo for apenas visualizar o conteúdo do arquivo, a opção mais indicada é que você veja pelo próprio visualizador do GitHub!!
+Para fazer isso, basta apenas clicar no arquivo, e seu conteúdo já será aberto no visualizador com todas formatações.
